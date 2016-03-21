7e81c01c4031efa4010a945d33f69a2d467df251
adrianescat <adrian.escat@gmail.com>
2016-03-21 19:02:18 -0300

    First commit. Uploading files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/***
+
+MercadoLibre Front-End Test by Adrian Escat
+
+***/
+
+;(function(){
+  var product_form = $("#product-form").form();
+  var carousel = $(".similar-products-carousel").carousel({
+    'limitPerPage': 3
+  });
+  var tabs = $("#product-tabs").tabs();
+})();
